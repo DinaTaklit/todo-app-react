38e68eb2d07865d128fbed73680724ee1ea973b7
DinaTaklit <ez_taklit@esi.dz>
2021-07-01 19:31:50 +0100

    feat: add TodoItem

---
+++ src/component/TodoItem.js
@@ -0,0 +1,10 @@
+function TodoItem(){
+    return(
+        <div className="todo-item">
+            <input type="checkbox"/>
+            <p>task 1</p>
+        </div>
+    )
+}
+
+export default TodoItem;
