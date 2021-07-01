883c20659735ecc95dcd2c1ee225216b00d01a1f
DinaTaklit <ez_taklit@esi.dz>
2021-07-02 00:45:51 +0100

    feat: change App funct to react class component

--- src/component/App.js
+++ src/component/App.js
@@ -1,17 +1,26 @@
 import '../style/App.css';
 import TodoItem from './TodoItem';
 import tasks from '../todoData.json'
+import React from 'react';
 
 
-function App() {
-  const todoItems = tasks.map(item =>  {
-    return <TodoItem key={item.id} task = {item}/>
-  });
-  return (
-    <div className="todo-list">
-      {todoItems}
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      todos: tasks
+    }
+  }
+  render(){
+    const todoItems = this.state.todos.map(item =>  {
+      return <TodoItem key={item.id} task = {item}/>
+    });
+    return (
+      <div className="todo-list">
+        {todoItems}
+      </div>
+    );
+  }
 }
 
 export default App;
